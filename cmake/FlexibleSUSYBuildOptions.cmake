88352f29f8bcf77d0e1ba5b0c21abf8e0ef57917
Dylan Harries <dylan.harries@adelaide.edu.au>
2018-01-22 07:07:10 +0100

    Add initial, incomplete CMake files

---
+++ cmake/FlexibleSUSYBuildOptions.cmake
@@ -0,0 +1,13 @@
+# define additional build options
+include(CMakeDependentOption)
+
+option(ENABLE_COLORS "Colored output (default: no)" OFF)
+option(ENABLE_COMPILER_WARNINGS "Enable compiler warnings" OFF)
+option(ENABLE_MASS_ERROR_CHECK "Check mass eigenvalue precision (default: no)" OFF)
+option(ENABLE_SILENT "Suppress all command line output (default: no)" OFF)
+cmake_dependent_option(ENABLE_VERBOSE "Verbose messages (default: no)" OFF
+  "NOT ENABLE_SILENT" OFF)
+option(ENABLE_TWO_SCALE_SOLVER "Enable use of the two-scale solver" ON)
+option(ENABLE_SEMI_ANALYTIC_SOLVER "Enable use of the semi-analytic solver" OFF)
+option(ENABLE_LATTICE_SOLVER "Enable use of the lattice solver" OFF)
+option(WARNINGS_AS_ERRORS "Treat all warnings as errors" OFF)
