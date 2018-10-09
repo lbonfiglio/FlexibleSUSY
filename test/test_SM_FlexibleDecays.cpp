b354314b57cbf0d026db433d013645608b8ae39d
Wojciech Kotlarski <wojciech.kotlarski@tu-dresden.de>
2018-10-09 21:46:40 +0200

    skeleton for tests of Higgs decay widths in the SM

---
+++ test/test_SM_FlexibleDecays.cpp
@@ -0,0 +1,43 @@
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE test_SM_FlexibleDecays
+
+#include <iostream>
+
+#include <boost/test/unit_test.hpp>
+
+#include "test_SM.hpp"
+#include "SM_two_scale_model.hpp"
+#include "SM_decays.hpp"
+
+// #include "wrappers.hpp"
+// #include "lowe.h"
+// #include "standard_model.hpp"
+
+using namespace flexiblesusy;
+
+BOOST_AUTO_TEST_CASE( test_SM_FlexibleDecays )
+{
+   
+   SM_input_parameters input;
+   input.LambdaIN = 0.25;
+   SM<Two_scale> m;
+   setup_SM_const(m, input);
+
+   m.calculate_DRbar_masses();
+
+   m.set_pole_mass_loop_order(1);
+   m.do_calculate_sm_pole_masses(true);
+   m.solve_ewsb_one_loop();
+   m.calculate_pole_masses();
+
+   if (m.get_problems().have_problem()) {
+      std::ostringstream ostr;
+      m.get_problems().print_problems(ostr);
+      BOOST_FAIL(ostr.str());
+   }
+
+   softsusy::QedQcd qedqcd;
+   SM_decays decays(m, qedqcd, true);
+   std::cout << decays.partial_width_hh_to_barFdFd(m, 2, 2) << '\n';
+}
