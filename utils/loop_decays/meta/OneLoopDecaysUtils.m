072d9b77abafc1667396ba89d69a812dfe4d672b
Dylan Harries <harries@ipnp.mff.cuni.cz>
2018-11-16 15:02:04 +0100

    Convert 3- and 4-vector boson vertices to SARAH notation

--- utils/loop_decays/meta/OneLoopDecaysUtils.m
+++ utils/loop_decays/meta/OneLoopDecaysUtils.m
@@ -135,7 +135,15 @@ CouplingToSARAHCpRules[] :=
      RuleDelayed[FeynArts`G[_][0][fields__]["d_"[FeynArts`KI1[i1_], FeynArts`KI1[i2_]]], SARAH`Cp[fields][SARAH`g[SARAHLorentzIndex[i1], SARAHLorentzIndex[i2]]]],
      RuleDelayed[FeynArts`G[_][0][fields__][FeynArts`Mom[i1_] - FeynArts`Mom[i2_]], SARAH`Cp[fields][SARAH`Mom[{fields}[[i1]]] - SARAH`Mom[{fields}[[i2]]]]],
      RuleDelayed[FeynArts`G[_][0][fields__][NonCommutativeMultiply[FeynArts`KI1[i1_], FormCalc`Private`ga[6]]], SARAH`Cp[fields][SARAH`LorentzProduct[SARAH`gamma[i1], SARAH`PR]]],
-     RuleDelayed[FeynArts`G[_][0][fields__][NonCommutativeMultiply[FeynArts`KI1[i1_], FormCalc`Private`ga[7]]], SARAH`Cp[fields][SARAH`LorentzProduct[SARAH`gamma[i1], SARAH`PL]]]
+     RuleDelayed[FeynArts`G[_][0][fields__][NonCommutativeMultiply[FeynArts`KI1[i1_], FormCalc`Private`ga[7]]], SARAH`Cp[fields][SARAH`LorentzProduct[SARAH`gamma[i1], SARAH`PL]]],
+     RuleDelayed[FeynArts`G[_][0][fields__]["d_"[FeynArts`KI1[i1_], FeynArts`KI1[i2_]] (FeynArts`Mom[i2_] - FeynArts`Mom[i1_])
+                                            + "d_"[FeynArts`KI1[i1_], FeynArts`KI1[i3_]] (FeynArts`Mom[i1_] - FeynArts`Mom[i3_])
+                                            + "d_"[FeynArts`KI1[i2_], FeynArts`KI1[i3_]] (FeynArts`Mom[i3_] - FeynArts`Mom[i2_])],
+                 SARAH`Cp[fields][SARAH`g[SARAHLorentzIndex[i1], SARAHLorentzIndex[i2]] (SARAH`Mom[{fields}[[i2]]] - SARAH`Mom[{fields}[[i1]]])
+                                  + SARAH`g[SARAHLorentzIndex[i1], SARAHLorentzIndex[i3]] (SARAH`Mom[{fields}[[i1]]] - SARAH`Mom[{fields}[[i3]]])
+                                  + SARAH`g[SARAHLorentzIndex[i2], SARAHLorentzIndex[i3]] (SARAH`Mom[{fields}[[i3]]] - SARAH`Mom[{fields}[[i2]]])]],
+     RuleDelayed[FeynArts`G[_][0][fields__]["d_"[FeynArts`KI1[i1_], FeynArts`KI1[i2_]] "d_"[FeynArts`KI1[i3_], FeynArts`KI1[i4_]]],
+                 SARAH`Cp[fields][SARAH`g[SARAHLorentzIndex[i1], SARAHLorentzIndex[i2]] SARAH`g[SARAHLorentzIndex[i3], SARAHLorentzIndex[i4]]]]
     };
 
 ToSARAHCouplings[expr_] := expr /. CouplingToSARAHCpRules[];
